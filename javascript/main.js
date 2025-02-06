db79905356bf82b8523e0ad07bce628bfc9c1425
Lohith.Elumalai <lohithhash@gmail.com>
2025-02-06 07:57:34 +0530

    up

--- javascript/main.js
+++ javascript/main.js
@@ -16,3 +16,11 @@ document.addEventListener("DOMContentLoaded", function () {
         });
     });
 });
+let counter = 1;
+    setInterval(() => {
+        document.getElementById('radio' + counter).checked = true;
+        counter++;
+        if (counter > 4) {
+            counter = 1;
+        }
+    }, 3000);
